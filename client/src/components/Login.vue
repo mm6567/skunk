e42e779134dbe8eb0af82287cd5dc446d2c156f1
Michael <mmm9363@C00.CORP.COM>
2018-01-11 11:24:11 -0800

    more work through workshop

---
+++ client/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+    <h3>Sign In</h3>
+    <input type="text" placeholder="Email"><br>
+    <button>Connection</button>
+    <p>No account yet?  Create one!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data: function() {
+    return{}
+  },
+  methods: {}
+}
+</script>
+
+<style scoped> /* scoped limits the CSS to this component only */
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+// <script>
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCvOMN38yY608mvf56qM7zp4VFDpg9sGRk",
+  authDomain: "workshoptest-a5697.firebaseapp.com",
+  databaseURL: "https://workshoptest-a5697.firebaseio.com",
+  projectId: "workshoptest-a5697",
+  storageBucket: "workshoptest-a5697.appspot.com",
+  messagingSenderId: "729359862464"
+};
+firebase.initializeApp(config);
+// </script>
 
 /* eslint-disable no-new */
 new Vue({
